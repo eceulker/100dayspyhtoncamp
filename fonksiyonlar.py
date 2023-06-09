b81e7b61bcaa6fd92d29a301aa766ef7b71cbfcd
Ece Ülker <97048304+eceulker@users.noreply.github.com>
2023-06-09 23:43:37 +0300

    Add files via upload

--- fonksiyonlar.py
+++ fonksiyonlar.py
@@ -18,5 +18,31 @@ def kwargs_example(**kwargs):
 print(kwargs_example(apple=100,banana=150,melon=200))
 print(kwargs_example())
 #PRACTİCAL_FUNCTİONS
-myList
+def divideNumber(number):
+    return number/2
+myList=[3,5,7,10,20,30]
+myResultList=[]
+for num in myList:
+    myResultList.append(divideNumber(num))
+print(myResultList)
+#map
+help(map)
+print(list(map(divideNumber,myList)))
+def controlString(string):
+    return "Ece" in string
+print(controlString("Ece Ulker"))
+print(controlString("Ulker"))
+myStringList=["Atil","Ece","Ece Ulker","Atlas"]
+print(list(map(controlString,myStringList)))
+#filter
+print(list(filter(controlString,myStringList)))
+#print(list(filter("l",myStringList))) wrong code
+#lambda
+multiplyLambda=lambda num:num*3
+print(type(multiplyLambda))
+numList=[10,20,30,40,50]
+print(list(map(lambda num:num/4,numList)))
+
+
+
 
